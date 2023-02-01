92901dec23187db1a72129bbd3ea60bbdf31f717
MrLately <94589563+MrLately@users.noreply.github.com>
2023-01-31 19:19:54 -0500

    Update bas.py

--- bas.py
+++ bas.py
@@ -20,30 +20,37 @@ pixels = neopixel.NeoPixel(
     pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
 )
 
-color = (0, 255, 0)
+color = (0, 0, 255)
 slide_wait = 0.05
 blink_wait = 0.5
+block_size = 3
 
 def slide_pattern():
-    for i in range(num_pixels):
+    for i in range(0, num_pixels, block_size):
         pixels.fill((0, 0, 0))
-        pixels[i] = color
+        for j in range(block_size):
+            if i + j < num_pixels:
+                pixels[i + j] = color
         pixels.show()
         time.sleep(slide_wait)
-    for i in range(num_pixels-1, -1, -1):
+    for i in range(num_pixels - block_size, -1, -block_size):
         pixels.fill((0, 0, 0))
-        pixels[i] = color
+        for j in range(block_size):
+            if i + j < num_pixels:
+                pixels[i + j] = color
         pixels.show()
         time.sleep(slide_wait)
 
 def blink_pattern():
     while True:
         pixels.fill((0, 0, 0))
-        pixels[0] = color
+        for i in range(block_size):
+            pixels[i] = color
         pixels.show()
         time.sleep(blink_wait)
         pixels.fill((0, 0, 0))
-        pixels[num_pixels - 1] = color
+        for i in range(num_pixels - block_size, num_pixels):
+            pixels[i] = color
         pixels.show()
         time.sleep(blink_wait)
 
